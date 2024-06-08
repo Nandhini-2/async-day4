b2d1fcf4472ffdf17720fd00109bc17cb7fbfa63
Nandhini-2 <68190579+Nandhini-2@users.noreply.github.com>
2024-06-08 18:36:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,22 +21,30 @@ return ele1;
 }
 
 var f_name = label_create("label","for","fname","FirstName");
+var f_name_break = break_create();
 var input_fname = input_create("input","type","text","id","fname");
 
 var m_name = label_create("label","for","mname","MiddleName");
+var m_name_break = break_create();
 var input_mname = input_create("input","type","text","id","mname");
 
 var l_name = label_create("label","for","lname","LastName");
+var l_name_break = break_create();
 var input_lname = input_create("input","type","text","id","lname");
 
 var pwd = label_create("label","for","pwd","Password");
+var pwd_break = break_create();
 var input_pwd = input_create("input","type","password","id","pwd");
 
 var file = label_create("label","for","mfile","Select a file:");
+var file_break = break_create();
 var input_file = input_create("input","type","file","id","mfile");
 
 var break_add = break_create();
 
 
-document.body.append(f_name,break_add,input_fname,break_add,m_name,break_add,break_add,input_mname,break_add,l_name,break_add,input_lname,break_add,pwd,break_add,input_pwd,break_add,
-    file,break_add,input_file
+document.body.append(f_name,f_name_break,input_fname,
+    m_name,m_name_break,input_mname,
+    l_name,l_name_break,input_lname,
+    pwd,pwd_break,input_pwd,
+    file,file_break,input_file)
